ba0a0b5664985e20f23c73feae65104c40da6ef3
Nicholas Valler <nvaller@newrelic.com>
2016-05-25 17:54:09 -0700

    fixes typo

--- db/migrate/20160526004815_create_posts.rb
+++ db/migrate/20160526004815_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title, null: false
       t.text :body, null:false
-      t.boolean :published: null: false
+      t.boolean :published, null: false
       t.datetime :published_at
 
       t.timestamps null: false
